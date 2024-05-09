dae6ce7b06a1fe28f8284e95cf6dc1112bc358a8
vace <nguyenvananh270897@gmail.com>
2024-05-09 17:16:03 +0700

    update css

--- iChar_App/src/main.js
+++ iChar_App/src/main.js
@@ -1,12 +1,15 @@
 
-
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
+
 import App from './App.vue'
 //import router from './router'
 
 const app = createApp(App)
+
 
 //app.use(router)
 
